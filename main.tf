2a7c99a805d916065a4da45e7f76457246f5fb75
JDL <jdluther2020@gmail.com>
2021-04-29 09:01:18 -0700

    Changing infrastrucutre components

--- main.tf
+++ main.tf
@@ -1,56 +1,64 @@
+# Terraform configuration
+# Create a module to manage AWS S3 buckets used to host static websites
+# https://learn.hashicorp.com/tutorials/terraform/module-create?in=terraform/modules
+
+module "website_s3_bucket" {
+  source = "./modules/aws-s3-static-website-bucket"
+
+  bucket_name = "jdl-terraform-bucket-04232021"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+# Terraform configuration
+# Use modules from the Terraform Registry to provision an example environment on AWS
+# https://learn.hashicorp.com/tutorials/terraform/module-use?in=terraform/modules
+
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-
-  required_version = "~> 0.14"
-
-  backend "remote" {
-    organization = "JDL-training"
-    workspaces {
-      name = "terraform-automation-with-github-actions"
+      source = "hashicorp/aws"
     }
   }
 }
-
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
 
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-resource "random_pet" "sg" {}
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
 
-resource "aws_instance" "web" {
-  ami                    = "ami-0742b4e673072066f"
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "2.12.0"
+
+  name           = "my-ec2-cluster"
+  instance_count = 2
+
+  ami                    = "ami-0c5204531f799e0c6"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
