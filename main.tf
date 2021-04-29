f7a692548db47df1b4b59bf1d54cc92e772c2c2a
JDL <jdluther2020@gmail.com>
2021-04-29 09:32:22 -0700

    Simplified resource creation

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-# Terraform configuration
-# Create a module to manage AWS S3 buckets used to host static websites
-# https://learn.hashicorp.com/tutorials/terraform/module-create?in=terraform/modules
-
-module "website_s3_bucket" {
-  source = "./modules/aws-s3-static-website-bucket"
-
-  bucket_name = "jdl-terraform-bucket-04292021"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
-# Terraform configuration
-# Use modules from the Terraform Registry to provision an example environment on AWS
-# https://learn.hashicorp.com/tutorials/terraform/module-use?in=terraform/modules
-
 terraform {
   required_providers {
     aws = {
@@ -39,36 +20,16 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.21.0"
+provider "random" {}
 
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+resource "random_pet" "name" {}
 
-  azs             = var.vpc_azs
-  private_subnets = var.vpc_private_subnets
-  public_subnets  = var.vpc_public_subnets
-
-  enable_nat_gateway = var.vpc_enable_nat_gateway
-
-  tags = var.vpc_tags
-}
-
-module "ec2_instances" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.12.0"
-
-  name           = "my-ec2-cluster"
-  instance_count = 2
-
-  ami                    = "ami-0c5204531f799e0c6"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.public_subnets[0]
+resource "aws_instance" "web" {
+  ami           = "ami-a0cfeed8"
+  instance_type = "t2.micro"
+  user_data     = file("init-script.sh")
 
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = random_pet.name.id
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,7 @@
-# Output definitions
-
-output "vpc_public_subnets" {
-  description = "IDs of the VPC's public subnets"
-  value       = module.vpc.public_subnets
+output "domain-name" {
+  value = aws_instance.web.public_dns
 }
 
-output "ec2_instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
-  value       = module.ec2_instances.public_ip
-}
-
-output "website_bucket_arn" {
-  description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
-}
-
-output "website_bucket_name" {
-  description = "Name (id) of the bucket"
-  value       = module.website_s3_bucket.name
-}
-
-output "website_bucket_domain" {
-  description = "Domain name of the bucket"
-  value       = module.website_s3_bucket.domain
+output "application-url" {
+  value = "${aws_instance.web.public_dns}/index.php"
 }
